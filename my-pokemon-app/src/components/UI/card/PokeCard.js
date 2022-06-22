837037290c2e97e59e49bf31e56a80539393180e
Azimjon Umarov <94437438+theazimjon@users.noreply.github.com>
2022-06-23 04:06:05 +0500

    left wrok: details spec and searching

--- my-pokemon-app/src/components/UI/card/PokeCard.js
+++ my-pokemon-app/src/components/UI/card/PokeCard.js
@@ -10,12 +10,30 @@ function PokeCard({pokeObject, shows, setShows}) {
         <div className={cardStyles.join(' ')} onClick={() => setShows(false)}>
             <div className={`${Styles.pokeCardContent} ${pokeObject.types[0].type.name} position-relative`} onClick={(e) => e.stopPropagation()}>
                 <img src={pokeObject.sprites.other.dream_world.front_default} alt="" className={`position-absolute top-50 start-0 translate-middle ${Styles.cardImage}`}  />
-                <div className={Styles.pokeCardId} >
-                    {pokeObject.abilities && pokeObject.abilities.map(val => !val.is_hidden ? <span className="badge text-bg-secondary m-1">{val.ability.name}</span> : null)}
-                </div>
                 <div className={`${Styles.cardBox}`}>
                     <span>{pokeObject.forms[0].url.split("/")[6]}</span>
                     <h2 className={Styles.cardName}>{pokeObject.forms[0].name}</h2>
+                    {/*<div className={Styles.pokeCardId} >*/}
+                    {/*    {pokeObject.abilities && pokeObject.abilities.map(val => !val.is_hidden ? <p>{val.ability.name}</p> : null)}*/}
+                    {/*</div>*/}
+
+                        <div style={{color: "#000", marginTop: "2rem"}}>
+                            <h3 className="text-center">Base stats</h3>
+                            <p>Type: {pokeObject.types[0].type.name}</p>
+                            <p>HP: {pokeObject.stats[0].base_stat}</p>
+                            <p>Attack: {pokeObject.stats[1].base_stat}</p>
+                            <p>Defense: {pokeObject.stats[2].base_stat}</p>
+                            <p>special-attack: {pokeObject.stats[3].base_stat}</p>
+                            <p>special-defence: {pokeObject.stats[4].base_stat}</p>
+                            <p>speed: {pokeObject.stats[5].base_stat}</p>
+                        </div>
+                    {/*<strong>*/}
+                        {/*Abilities:*/}
+                    {/*</strong>*/}
+                    {/*<ul>*/}
+                    {/*    <li>{pokeObject.abilities[0].ability.name}</li>*/}
+                    {/*    <li>{pokeObject.abilities[1].ability.name}</li>*/}
+                    {/*</ul>*/}
                 </div>
             </div>
         </div>
